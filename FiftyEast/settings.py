5bea9c2310b3a353fad38edb0c9611bd926b5b59
Devon Wolsleger <devonrw@me.com>
2022-12-13 10:44:43 -0700

    Deployment Commit

--- FiftyEast/settings.py
+++ FiftyEast/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,16 +78,23 @@ WSGI_APPLICATION = 'FiftyEast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'fiftyeast',
-    'USER': 'postgres',
-    'PASSWORD': 'admin',
-    'HOST': 'localhost',
-    'PORT': '5433'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+#Before
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'fiftyeast',
+#     'USER': 'postgres',
+#     'PASSWORD': 'admin',
+#     'HOST': 'localhost',
+#     'PORT': '5433'
+#     }
+# }
 
 
 # Password validation
